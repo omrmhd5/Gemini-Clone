6a78e570728d5c72ec0b42dfed943805186a21cf
omrmhd5 <omrmhd54@gmail.com>
2025-02-01 00:31:10 +0200

    Google API

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ const clearChatButton = document.getElementById("deleteButton");
 //State Variables
 let currentUserMessage = null;
 let isGeneratingResponse = false;
+
+const GOOGLE_API_KEY = "AIzaSyCs_1-zN1OiNe2V774pbROTee_eQYY6Pcc";
+const API_REQUEST_URL = `"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GOOGLE_API_KEY}`;
